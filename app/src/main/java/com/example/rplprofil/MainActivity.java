ea3381f223bcf553a93a0363e6710468f01ab7fb
auliakbark <auliakbark@student.uns.ac.id>
2022-11-23 19:32:36 +0700

    Kelompok 4 TI A

--- app/src/main/java/com/example/rplprofil/MainActivity.java
+++ app/src/main/java/com/example/rplprofil/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.rplprofil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button akbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        akbar = findViewById(R.id.button4);
+
+        akbar.setOnClickListener(view -> {
+            Intent akbarIntent = new Intent(getApplicationContext(), ProfilAkbar.class);
+            startActivity(akbarIntent);
+        });
     }
 }
